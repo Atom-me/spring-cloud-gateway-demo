4845d50390fecc17ae3b0db041b444af4d2c04a2
Atom <sarming@126.com>
2023-05-04 15:48:42 +0800

    add comment

--- gateway/src/main/java/com/atom/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/atom/gateway/GatewayApplication.java
@@ -3,6 +3,8 @@ package com.atom.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ */
 @SpringBootApplication
 public class GatewayApplication {
 
